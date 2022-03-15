114841b5625ff2192b50203120cc95178b1ea883
Deependra <70470040+deepak685@users.noreply.github.com>
2022-03-16 00:56:13 +0530

    Update route.js

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,10 +1,30 @@
 const express = require('express');
-
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    res.send('My first ever api!')
-});
+//--------------------------------------------------
+const authorController = require('../controller/auhtorController')
+const blogController = require('../controller/blogController')
+const mid1 = require('../middleware/tokenmiddleware')
+
+
+
+
+
+
+//=---------------------------------------
+router.post("/Authors", authorController.createAuthor)
+
+router.post('/blogs', mid1.mid1, blogController.Blogs)
+
+router.get('/getBlog', mid1.mid1, blogController.getBlogs)
+
+router.put('/blogs/:blogId', mid1.mid2, blogController.updating)
+
+router.delete('/blogs/:blogId', mid1.mid2, blogController.deleting)
+
+router.delete('/blogs', mid1.mid1, blogController.specificdeleting)
+
+router.post('/login', blogController.loginAuthor)
+
 
 module.exports = router;
-// adding this comment for no reason
