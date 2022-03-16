7b7b0f95535125c1b7b184917624d1e16b73078d
Deependra <70470040+deepak685@users.noreply.github.com>
2022-03-16 12:09:27 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
-const express = require('express');
-var bodyParser = require('body-parser');
+const express = require("express")
+const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
+const jwt = require("jsonwebtoken")
+const route = require('./routes/route');
+const { json } = require("express/lib/response");
+const url = "mongodb+srv://Deependra1999:Z1ZWVlMvcAFQsu2u@cluster0.4nkid.mongodb.net/Blogsite"
 
-const route = require('./routes/route.js');
-
-const app = express();
+const app = express()
+app.use(express.json())
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/', route);
+mongoose.connect(url, { useNewUrlparser: true })
+    .then(() => console.log("MongoDb is connected"))
+    .catch(err => console.log(err))
 
-app.listen(process.env.PORT || 3000, function() {
-    console.log('Express app running on port ' + (process.env.PORT || 3000))
+app.use('/', route)
+
+
+app.listen(3000, () => {
+    console.log('Express app running on port ' + 3000)
 });
